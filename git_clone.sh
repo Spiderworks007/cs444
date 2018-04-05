e1b04e285a48548a0a5fb530ef96e75016b25118
Spiderworks007 <spiderworks007@gmail.com>
2018-04-05 17:16:45 -0500

    /Users/Spider/Desktop/444/

---
+++ git_clone.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+START=$HOME
+ 
+# change your directory to command line if passed
+# otherwise use home directory
+[ $# -eq 1 ] && START=$1 
+ echo "                                             Test_1 $START"
+if [ ! -d $START ]
+then
+	echo "$START not a directory!"
+	exit 1
+fi
+     echo  The number of files: 
+     ls -l | wc -l
+echo "git int"
+    git init
+echo "git int"
+    git add $1
+    git status
+echo "git commit"
+    git commit -m "$1"
